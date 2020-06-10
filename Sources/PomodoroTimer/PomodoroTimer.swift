37a50a4dfc3971c0888142495315d72fb73a0204
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-06-10 14:30:08 +0100

    Create initializers and duration properties
    Creating properties for focus, short and long break duration. They have default values (25,5,15).
    Timer can be initialized using just init(), using the default values, or can be initialized passing the durations.
    Initializing with wrong properties going to fail the initialization and return nil.

--- Sources/PomodoroTimer/PomodoroTimer.swift
+++ Sources/PomodoroTimer/PomodoroTimer.swift
@@ -1,3 +1,59 @@
-struct PomodoroTimer {
-    var text = "Hello, World!"
+//  MIT License
+//
+//  Copyright (c) 2020 Filipe Jorge
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+public class PomodoroTimer {
+    
+    // MARK: - Initializers
+    public init() { }
+    
+    public init?(focus: Int, short: Int, long: Int) {
+        guard focus > 0, short > 0, long > 0 else {
+            return nil
+        }
+        
+        _focusDuration = focus
+        _shortBreakDuration = short
+        _longBreakDuration = long
+    }
+    
+    // MARK: - Properties
+    private var _focusDuration: Int = 25
+    
+    public var focusDuration: Int {
+        return _focusDuration
+    }
+    
+    public var _shortBreakDuration: Int = 5
+    
+    public var shortBreakDuration: Int {
+        return _shortBreakDuration
+    }
+    
+    private var _longBreakDuration: Int = 15
+    
+    public var longBreakDuration: Int {
+        return _longBreakDuration
+    }
+    
+    
+    
 }
