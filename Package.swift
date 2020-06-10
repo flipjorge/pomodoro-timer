a52053f42607df70ba6511ad86578e7cdd46b91d
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-06-10 16:04:56 +0100

    Set dependecy to SecondsTimer library

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PomodoroTimer",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,15 +16,14 @@ let package = Package(
             targets: ["PomodoroTimer"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/flipjorge/seconds-timer.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PomodoroTimer",
-            dependencies: []),
+            dependencies: ["SecondsTimer"]),
         .testTarget(
             name: "PomodoroTimerTests",
             dependencies: ["PomodoroTimer"]),
